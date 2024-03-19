356faf40ba803c7f0063ccee4ce05149c6cbd168
Jaime Moscoso <jaime.r.moscoso@gmail.com>
2024-03-19 10:23:48 -0500

    Update OCI_EDL.php

--- OCI_EDL.php
+++ OCI_EDL.php
@@ -2,6 +2,7 @@
 $url = "https://docs.oracle.com/iaas/tools/public_ip_ranges.json";
 $json_data = file_get_contents($url);
 $oci = json_decode($json_data);
+//HTML template if you need pretty ui
 //echo '<html><head><meta name="color-scheme" content="ligh dark"></head><body><pre style="word-wrap: break-word; white-space: pre-wrap;">';
 foreach ($oci->regions as $region) {
     foreach ($region->cidrs as $cidr) {
